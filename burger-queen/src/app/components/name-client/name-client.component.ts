929eea1c2ad62c3a946fa7d9e786230609539867
Myriam <myriam.rivera.santiago@gmail.com>
2022-09-14 21:56:34 -0500

    se crea componente nameClient

---
+++ burger-queen/src/app/components/name-client/name-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-client',
+  templateUrl: './name-client.component.html',
+  styleUrls: ['./name-client.component.css']
+})
+export class NameClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
