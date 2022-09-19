df0950e653b5a8438b7c2e821f910b11569102b7
daniandp <andradegsp@gmail.com>
2022-09-19 12:15:46 -0500

    cambio de rutas de welcome al nameClient y de ahí al view waiter order

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ViewWaiterOrderComponent } from './components/view-waiter-order/view-waiter-order.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NameClientComponent } from './components/name-client/name-client.component';
+import { ViewWaiterOrderComponent } from './components/view-waiter-order/view-waiter-order.component';
 
 const routes: Routes = [
 { path: '', pathMatch: 'full', redirectTo: '/welcome' },
 { path: 'welcome', component: WelcomeComponent },
+{ path: 'nameClient', component: NameClientComponent },
 { path: 'menuWaiter', component: ViewWaiterOrderComponent },
 ];
 
